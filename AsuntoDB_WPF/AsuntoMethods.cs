c177c810583859b6e0fba3990c6893193d066996
Antti Minkkinen <g7786@student.jamk.fi>
2014-12-12 10:58:10 +0200

    Initial commit

---
+++ AsuntoDB_WPF/AsuntoMethods.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsuntoDB_WPF
+{
+    public partial class MainWindow : Window
+    {
+        private int valittuAsuntoAvain = -1;
+        private int valittuAsuntoIndex = -1;
+
+        private void LataaAsunnot()
+        {
+            lbAsuntoLista.ItemsSource = null;
+
+            var result = from t in db.Asunto
+                         orderby t.Osoite
+                         select t;
+            lbAsuntoLista.ItemsSource = result.ToList();
+            lbAsuntoLista.Items.Refresh();
+            lbAsuntoLista.DisplayMemberPath = "Osoite";
+            lbAsuntoLista.SelectedValuePath = "Avain";
+        }
+
+
+        private void lbAsuntoLista_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            paivitaValittuAsunto();
+            naytaAsunto();
+        }
+
+        private void naytaAsunto()
+        {
+            if (valittuAsuntoIndex == -1)
+            {
+                txtAsuntoAsuntonumero.Text = "";
+                txtAsuntoOsoite.Text = "";
+                txtAsuntoPintaala.Text = "";
+                txtAsuntoHuonemaara.Text = "";
+                chkOmistusasunto.IsChecked = false;
+
+                txtAsuntoAsuntonumero.IsEnabled = false;
+                txtAsuntoOsoite.IsEnabled = false;
+                txtAsuntoPintaala.IsEnabled = false;
+                txtAsuntoHuonemaara.IsEnabled = false;
+                chkOmistusasunto.IsEnabled = false;
+                cbAsuntotyyppi.IsEnabled = false;
+
+                return;
+            }
+
+            var result = from t in db.Asunto
+                         where t.Avain == valittuAsuntoAvain
+                         select t;
+            var valittu = result.FirstOrDefault();
+
+            if (valittu != null)
+            {
+                sbItem.Content = string.Format("Valittu asunto {0}", valittu.Osoite);
+                txtAsuntoAsuntonumero.Text = "";
+                txtAsuntoOsoite.Text = "";
+                txtAsuntoPintaala.Text = "";
+                txtAsuntoHuonemaara.Text = "";
+                chkOmistusasunto.IsChecked = false;
+            }
+        }
+
+        private void paivitaValittuAsunto()
+        {
+            try
+            {
+                valittuAsuntotyyppiKoodi = int.Parse(lbAsuntotyyppiLista.SelectedValue.ToString());
+            }
+            catch (Exception e)
+            {
+                valittuAsuntotyyppiKoodi = -1;
+            }
+            valittuAsuntotyyppiIndex = lbAsuntotyyppiLista.SelectedIndex;
+        }
+    }
+}
---
+++ AsuntoDB_WPF/AsuntotyyppiMethods.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsuntoDB_WPF
+{
+    public partial class MainWindow : Window
+    {
+        private int valittuAsuntotyyppiKoodi = -1;
+        private int valittuAsuntotyyppiIndex = -1;
+
+        private void LataaAsuntotyypit()
+        {
+            lbAsuntotyyppiLista.ItemsSource = null;
+
+            var result = from t in db.Asuntotyyppi
+                         orderby t.Selite
+                         select t;
+            lbAsuntotyyppiLista.ItemsSource = result.ToList();
+            lbAsuntotyyppiLista.Items.Refresh();
+            lbAsuntotyyppiLista.DisplayMemberPath = "Selite";
+            lbAsuntotyyppiLista.SelectedValuePath = "Koodi";
+        }
+
+
+        private void lbAsuntotyyppiLista_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            paivitaValittuAsuntotyyppi();
+            naytaAsuntotyyppi();
+        }
+
+        private void naytaAsuntotyyppi()
+        {
+            if (valittuAsuntotyyppiIndex == -1)
+            {
+                txtAsuntotyyppiSelite.Text = "";
+                txtAsuntotyyppiSelite.IsEnabled = false;
+                return;
+            }
+
+            var result = from t in db.Asuntotyyppi
+                         where t.Koodi == valittuAsuntotyyppiKoodi
+                         select t;
+            var valittu = result.FirstOrDefault();
+
+            if (valittu != null)
+            {
+                sbItem.Content = string.Format("Valittu asuntotyyppi {0}", valittu.Selite);
+                txtAsuntotyyppiSelite.Text = valittu.Selite;
+            }
+        }
+
+        private void paivitaValittuAsuntotyyppi()
+        {
+            try
+            {
+                valittuAsuntotyyppiKoodi = int.Parse(lbAsuntotyyppiLista.SelectedValue.ToString());
+            }
+            catch (Exception e)
+            {
+                valittuAsuntotyyppiKoodi = -1;
+            }
+            valittuAsuntotyyppiIndex = lbAsuntotyyppiLista.SelectedIndex;
+        }
+    }
+}
